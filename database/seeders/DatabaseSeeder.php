d97cbd4090521342638795a28615b6925472e77e
Marcelo Henrique de Freitas <marcelo.freitas@campus.unimontes.int>
2024-01-18 07:12:42 -0300

    Iniciado o projeto do curso, modelado o banco e criadas factories para testes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        // $this->call([
+        //     UserSeeder::class,
+
+        // ]);
+
+        User::factory(4)->create();
+        Category::factory(30)->create();
+        Task::factory(100)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
